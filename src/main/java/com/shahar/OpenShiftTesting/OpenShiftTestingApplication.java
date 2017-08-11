bce5a3d084bbaaf3d78ff799a0d8ee73a1889fff
shahar.bar <shahar.bar@travelport.com>
2017-08-11 19:51:49 +0100

    Initial rev

---
+++ src/main/java/com/shahar/OpenShiftTesting/OpenShiftTestingApplication.java
@@ -0,0 +1,12 @@
+package com.shahar.OpenShiftTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenShiftTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenShiftTestingApplication.class, args);
+	}
+}
