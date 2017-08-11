9a43ac49fc7244b94d37333adc1fecff94ca25d6
shahar.bar <shahar.bar@travelport.com>
2017-08-11 20:01:23 +0100

    Added a bit of code

--- src/main/java/com/shahar/OpenShiftTesting/OpenShiftTestingApplication.java
+++ src/main/java/com/shahar/OpenShiftTesting/OpenShiftTestingApplication.java
@@ -2,11 +2,20 @@ package com.shahar.OpenShiftTesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OpenShiftTestingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenShiftTestingApplication.class, args);
+	}
+	
+	@RequestMapping(path="/hello")
+	public String hello(@RequestParam(defaultValue="Shahar") String name) {
+		return "Hello " + name + "!";
 	}
 }
